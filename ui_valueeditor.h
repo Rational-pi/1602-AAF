98b76ed1887647048653322539f77a1f8b282862
Rational-pi <1salmieri.nicolas@gmail.com>
2018-02-08 20:19:06 +0100

    Ui_ValueEditor fonctional

--- ui_valueeditor.h
+++ ui_valueeditor.h
@@ -17,53 +17,71 @@ public:
     Ui_ValueEditor(App *app,Data *EditedVal):
         Ui_Base(app),
         EditedVal(EditedVal),
-        currentState(state::drawVal){
+        currentState(state::Value),
+        pitch(1){
         Data testVal=0;
         testVal+=0.01;
-        if(testVal==0.01)/*not integer*/
-            pitch=0.1;
-        else pitch=1;
+        DataIsInteger=testVal!=0.01;
     }
     virtual void compute(){}
     virtual void render(){
         app->lcd.clear();
         app->lcd.setCursor(0,0);
         switch (currentState) {
-        case state::drawVal:
-            app->lcd.print(*EditedVal);
+        case state::Value:
+            app->lcd.print("Value: ");
+            app->lcd.setCursor(0,1);
+            app->lcd.print(*EditedVal,10);
             break;
-        case state::menu:
-            app->lcd.print("Action implemented");
+        case state::pitch:
+            app->lcd.print("Pitch: ");
+            app->lcd.setCursor(0,1);
+            app->lcd.print(pitch,15);
+            break;
+        case state::saveExit:
+            app->lcd.print("Exit?: NO");
             break;
         }
     }
     virtual void HandleClick(){
         switch (currentState) {
-        case state::drawVal:
-            currentState=state::menu;
+        case state::Value:
+            currentState=state::pitch;
             break;
-        case state::menu:
-            currentState=state::drawVal;
+        case state::pitch:
+            currentState=state::saveExit;
+            break;
+        case state::saveExit:
+            currentState=state::Value;
             break;
         }
         needRendering=true;
     }
     virtual void HandleDelta(int8_t delta){
         switch (currentState) {
-        case state::drawVal:
+        case state::Value:
             *EditedVal+=pitch*delta;
             break;
-        case state::menu:
+        case state::pitch:
+            pitch*=delta>0?2:0.5;
+            if(DataIsInteger&&pitch<=1)pitch=1;
+            break;
+        case state::saveExit:
+            exit();
             break;
         }
         needRendering=true;
     }
+
+
     float pitch;
 private:
+    bool DataIsInteger;
     Data *EditedVal;
     enum class state{
-        drawVal,
-        menu,
+        Value,
+        pitch,
+        saveExit,
     }currentState;
 };
 
